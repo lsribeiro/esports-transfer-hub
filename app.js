f9eee51b6ba11b7604ef4d239778a5d28856aa2e
Lucas Ribeiro <lucas@lsribeiro.com>
2019-09-25 10:14:47 -0300

    Changed path for serving index.html

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express    = require('express');
-const path 		 = require('path');
 const cookieParser = require('cookie-parser');
 
 const { connect_db } = require('./config/db');
@@ -15,8 +14,10 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/transfers', require('./routes/api/transfers'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-app.get('/*', function (req, res) {
-  	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+const root = require('path').join(__dirname, 'client', 'build');
+app.use(express.static(root));
+app.get('*', (req, res) => {
+	res.sendFile('index.html', { root });
 });
 
 app.listen(process.env.PORT, () => console.log(`Server running... Port: ${process.env.PORT}`));
