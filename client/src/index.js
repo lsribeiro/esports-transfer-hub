c57d1c1e51df431ada548a42171a14395cfc9141
Lucas Ribeiro <lucas@lsribeiro.com>
2019-10-05 18:25:56 -0300

    Added redux, redux-form and redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './redux/reducers/rootReducer';
+
+const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'));
---
+++ client/src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+	transfers: []
+}
+
+export default function rootReducert(state=initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
